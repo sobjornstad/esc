22056d4c66ca9c0b4de21ee57a097eff9a5a50fc
Soren I. Bjornstad <contact@sorenbjornstad.com>
2019-09-26 19:42:35 -0500

    scroll history window when lines no longer fit
    There's probably a cleaner way to do this in curses, but we'll be adding
    more options related to that in the future. For now we'll just only
    select the number of lines that fit.

--- display.py
+++ display.py
@@ -138,11 +138,15 @@ class HistoryWindow(Window):
         self.window.clear()
         self.window.border()
         self.window.addstr(0, 13, "History")
-        for yposn, description in enumerate(self.operations, 1):
+
+        available_lines = self.height - 2
+        visible_operations = self.operations[-available_lines:]
+        for yposn, description in enumerate(visible_operations, 1):
             max_item_width = self.width - 2
             if len(description) > max_item_width:
                 description = description[:max_item_width-3] + '...'
             self.window.addstr(yposn, 1, description)
+
         super().refresh()
 
 
